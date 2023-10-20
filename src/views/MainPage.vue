5a900d9255bd5bca799b52ade6d174393ca64a41
lastfusion <iamvishsurya@gmail.com>
2023-10-20 16:06:48 +0530

    dsf

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -121,7 +121,7 @@ export default {
                 <td class="column-affinity centered" v-for="aff in persona.affinities" :key="aff">{{ aff }}</td>
                 <td class="column-fuse">
                     <router-link class="fusion-redirect" v-if="!persona.treasure"
-                        :to="{ name: 'Fusion', params: { name: serializeName(persona.name) } }" style="background-color: blueviolet; border-radius: 2px;">
+                        :to="{ name: 'Fusion', params: { name: serializeName(persona.name) } }" style="background-color: rgb(43, 86, 226); border-radius: 2px;">
                         Fuse&gt;
                     </router-link>
                 </td>

6cec9aa993afe8c09679f44df7c9ba4096964eb0
lastfusion <iamvishsurya@gmail.com>
2023-10-21 13:46:17 +0530

    s

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -114,7 +114,7 @@ export default {
                 <td class="column-name">
                     <router-link class="persona-redirect"
                         :to="{ name: 'Persona', params: { name: serializeName(persona.name) } }"
-                        style="color: #0400ff; font-style: bold;">
+                        style="color: #d400ff; font-style: bold;">
                         {{ persona.name }}
                     </router-link>
                 </td>

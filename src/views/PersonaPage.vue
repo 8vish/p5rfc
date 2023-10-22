d66ce3cbc6eef6871dd30ef8edb1aaa21b1d17e0
lastfusion <iamvishsurya@gmail.com>
2023-10-22 19:41:15 +0530

    sd

--- src/views/PersonaPage.vue
+++ src/views/PersonaPage.vue
@@ -122,7 +122,7 @@ export default {
     <table id="persona-skills">
         <thead>
             <tr>
-                <th class="column-header" colspan="5" style="text-align: center; font-weight: 700; font-size: 1rem; color: black;">Skills</th>
+                <th class="column-header" colspan="5" style="text-align: center; font-weight: 800; font-size: 1.5rem; color: black; font-style: italic;">Skills</th>
             </tr>
             <tr>
                 <th class="column-header column-skill-level">Level</th>
